41ced8d765a46cf3a6ed9eae4ec00a8c721bd1e0
Daniel <dorellanaff>
2024-07-19 14:29:02 +0000

    fix model

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run('app.main:app', host="0.0.0.0", port=8080, reload=False,
+    uvicorn.run('app.main:app', host="0.0.0.0", port=8080, reload=True,
         #ssl_keyfile='certs/localhost+2-key.pem',
         #ssl_certfile='certs/localhost+2.pem'
 )
